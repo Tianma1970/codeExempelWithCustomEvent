c23b8294d7baa5de9e9d1ab7ee4173d82bd300d6
Tillmann Weimer <tillmann1970@gmail.com>
2020-11-04 18:57:50 +0100

    created empty customEvent

--- app.js
+++ app.js
@@ -3,23 +3,21 @@ city = "Malmo"
 metropol = "Copenhagen"
 ;(function () {
   //querySelector for button
-  const button = document.querySelector(".button")
 
   //two ways to call the global variable which is outside the IIFE (in console)
-  console.log("I live in " + city + " and we love our neighbor city, which is " + metropol)
-  console.log(`my hometown is ${city} and we love our neighbor city, which is ${metropol}`)
+  console.log("I live in " + city + " and we love our neighbour city, which is " + metropol)
+  console.log(`my hometown is ${city} and we love our neighbour city, which is ${metropol}`)
   // call the global variable which is outside the IIFE on the page
   const text = document.querySelector(".myText")
   text.append("My hometown is " + city)
 
   //create a customEvent triggered by button click
-  button.addEventListener("click", () => {
-    const event = new CustomEvent("buttonChange", {
-      //detail: xxxxx
-    })
-    document.dispatchEvent(event)
-    document.addEventListener("buttonChange", () => {
-      console.log("TEST")
-    })
+  const button = document.querySelector(".button")
+
+  const event = new CustomEvent("buttonChange", {
+    detail: {}
   })
+  document.dispatchEvent(event)
+
+  button.addEventListener("buttonChange", () => {})
 })()
